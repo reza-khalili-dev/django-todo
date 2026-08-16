ba627cc32aaf12e78c3f396fb096b20f11a24682
reza-khalili-dev <arsalankhalili688@gmail.com>
2026-08-16 19:07:43 +0330

    chore: bump version to 1.1.0

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,7 @@ STATICFILES_DRIS = [BASE_DIR / "static"]  # for static files
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Version
+VERSION = "1.1.0"
